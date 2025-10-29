113a17d475586b9c3cde962ebb4962d02338ccc1
bobrov4709 <bobrov4709@mail.ru>
2025-10-29 14:29:33 +0300

    Add section attributes

--- src/layouts/Section/Section.jsx
+++ src/layouts/Section/Section.jsx
@@ -14,11 +14,18 @@ const Section = (props) => {
   return (
     <section
       className={classNames(className, 'section container')}
-      arai-label={titleId}
+      aria-label={titleId}
     >
       <header className="section__header">
         <div className="section__info">
-          <h2 className="section__title h3"></h2>
+          <h2 className="section__title h3" id={titleId}>
+              {title}
+          </h2>
+            {description && (
+                <div className="section__description">
+                <p>{description}</p>
+                </div>
+            )}
         </div>
         {actions && (
           <div className="section__actions">{actions}</div>
