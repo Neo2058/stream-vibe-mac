4b78bffc5905e5316553d514c060624b982abc74
Vyacheslav Bobrov <bobrov4709@mail.ru>
2025-11-11 10:15:09 +0300

    Page movies

---
+++ src/sections/MoviesBanner/MoviesBanner.jsx
@@ -0,0 +1,21 @@
+import './MoviesBanner.scss'
+import classNames from 'classnames'
+
+const MoviesBanner = (props) => {
+  const titleId = 'movies-banner-title'
+
+
+
+  return (
+    <section
+      className="movies-banner container"
+      aria-labelledby={titleId}
+    >
+      <h1 className="visually-hidden" id={titleId}>
+        Movies & Shows
+      </h1>
+    </section>
+  )
+}
+
+export default MoviesBanner
